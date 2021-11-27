276bdbf53ebe98bccb36a093b0bd63d945c2c50e
iNEShiro <68826694+TBxHiro@users.noreply.github.com>
2021-11-27 13:35:59 +0600

    centerscreen

--- oop2Project/Form6.Designer.cs
+++ oop2Project/Form6.Designer.cs
@@ -54,17 +54,19 @@ namespace oop2Project
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(233, 33);
+            this.label1.Location = new System.Drawing.Point(175, 27);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(371, 37);
+            this.label1.Size = new System.Drawing.Size(293, 30);
             this.label1.TabIndex = 0;
             this.label1.Text = "Bus Booking Site.Com  ";
             // 
             // butbookticket
             // 
-            this.butbookticket.Location = new System.Drawing.Point(187, 384);
+            this.butbookticket.Location = new System.Drawing.Point(140, 312);
+            this.butbookticket.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.butbookticket.Name = "butbookticket";
-            this.butbookticket.Size = new System.Drawing.Size(99, 39);
+            this.butbookticket.Size = new System.Drawing.Size(74, 32);
             this.butbookticket.TabIndex = 2;
             this.butbookticket.Text = "Book Ticket";
             this.butbookticket.UseVisualStyleBackColor = true;
@@ -72,9 +74,10 @@ namespace oop2Project
             // 
             // btncancel
             // 
-            this.btncancel.Location = new System.Drawing.Point(517, 375);
+            this.btncancel.Location = new System.Drawing.Point(388, 305);
+            this.btncancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btncancel.Name = "btncancel";
-            this.btncancel.Size = new System.Drawing.Size(111, 35);
+            this.btncancel.Size = new System.Drawing.Size(83, 28);
             this.btncancel.TabIndex = 3;
             this.btncancel.Text = "Cancel Ticket ";
             this.btncancel.UseVisualStyleBackColor = true;
@@ -82,115 +85,129 @@ namespace oop2Project
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(58, 118);
+            this.label2.Location = new System.Drawing.Point(44, 96);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 4;
             this.label2.Text = "From     : ";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(131, 118);
+            this.textBox1.Location = new System.Drawing.Point(98, 96);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(158, 22);
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
             this.textBox1.TabIndex = 5;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(58, 167);
+            this.label3.Location = new System.Drawing.Point(44, 136);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.Size = new System.Drawing.Size(53, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "To         : ";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(131, 164);
+            this.textBox2.Location = new System.Drawing.Point(98, 133);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(158, 22);
+            this.textBox2.Size = new System.Drawing.Size(120, 20);
             this.textBox2.TabIndex = 7;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(131, 206);
+            this.textBox3.Location = new System.Drawing.Point(98, 167);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(158, 22);
+            this.textBox3.Size = new System.Drawing.Size(120, 20);
             this.textBox3.TabIndex = 8;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(9, 206);
+            this.label4.Location = new System.Drawing.Point(7, 167);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(117, 17);
+            this.label4.Size = new System.Drawing.Size(88, 13);
             this.label4.TabIndex = 9;
             this.label4.Text = "Date of journey : ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(50, 293);
+            this.label5.Location = new System.Drawing.Point(38, 238);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.Size = new System.Drawing.Size(58, 13);
             this.label5.TabIndex = 10;
             this.label5.Text = "Seat No  : ";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(131, 293);
+            this.textBox4.Location = new System.Drawing.Point(98, 238);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(158, 22);
+            this.textBox4.Size = new System.Drawing.Size(120, 20);
             this.textBox4.TabIndex = 11;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(517, 161);
+            this.dataGridView1.Location = new System.Drawing.Point(388, 131);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 196);
+            this.dataGridView1.Size = new System.Drawing.Size(180, 159);
             this.dataGridView1.TabIndex = 12;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(58, 252);
+            this.label6.Location = new System.Drawing.Point(44, 205);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(64, 17);
+            this.label6.Size = new System.Drawing.Size(49, 13);
             this.label6.TabIndex = 13;
             this.label6.Text = "Bus       :";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(131, 252);
+            this.textBox5.Location = new System.Drawing.Point(98, 205);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(158, 22);
+            this.textBox5.Size = new System.Drawing.Size(120, 20);
             this.textBox5.TabIndex = 14;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(70, 340);
+            this.label7.Location = new System.Drawing.Point(52, 276);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(52, 17);
+            this.label7.Size = new System.Drawing.Size(40, 13);
             this.label7.TabIndex = 15;
             this.label7.Text = "Cost   :";
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(128, 340);
+            this.textBox6.Location = new System.Drawing.Point(96, 276);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(158, 22);
+            this.textBox6.Size = new System.Drawing.Size(120, 20);
             this.textBox6.TabIndex = 16;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(643, 97);
+            this.button1.Location = new System.Drawing.Point(482, 79);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 38);
+            this.button1.Size = new System.Drawing.Size(86, 31);
             this.button1.TabIndex = 17;
             this.button1.Text = "Profile ";
             this.button1.UseVisualStyleBackColor = true;
@@ -199,9 +216,10 @@ namespace oop2Project
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(682, 405);
+            this.button3.Location = new System.Drawing.Point(512, 329);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 33);
+            this.button3.Size = new System.Drawing.Size(56, 27);
             this.button3.TabIndex = 19;
             this.button3.Text = "Logout ";
             this.button3.UseVisualStyleBackColor = true;
@@ -209,10 +227,10 @@ namespace oop2Project
             // 
             // Form6
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox6);
@@ -231,7 +249,9 @@ namespace oop2Project
             this.Controls.Add(this.btncancel);
             this.Controls.Add(this.butbookticket);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form6";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form6";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
--- oop2Project/Form7.Designer.cs
+++ oop2Project/Form7.Designer.cs
@@ -57,17 +57,19 @@ namespace oop2Project
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(226, 105);
+            this.label1.Location = new System.Drawing.Point(170, 85);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.Size = new System.Drawing.Size(47, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Email : ";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(309, 105);
+            this.textBox1.Location = new System.Drawing.Point(232, 85);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(202, 22);
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
@@ -75,9 +77,10 @@ namespace oop2Project
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(436, 143);
+            this.button1.Location = new System.Drawing.Point(327, 116);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 28);
+            this.button1.Size = new System.Drawing.Size(56, 23);
             this.button1.TabIndex = 2;
             this.button1.Text = "Send ";
             this.button1.UseVisualStyleBackColor = true;
@@ -87,17 +90,19 @@ namespace oop2Project
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(211, 205);
+            this.label2.Location = new System.Drawing.Point(158, 167);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.Size = new System.Drawing.Size(56, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "OTP      : ";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(309, 205);
+            this.textBox2.Location = new System.Drawing.Point(232, 167);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(202, 22);
+            this.textBox2.Size = new System.Drawing.Size(152, 20);
             this.textBox2.TabIndex = 4;
             this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
             // 
@@ -105,25 +110,28 @@ namespace oop2Project
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(166, 273);
+            this.label3.Location = new System.Drawing.Point(124, 222);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(116, 17);
+            this.label3.Size = new System.Drawing.Size(91, 15);
             this.label3.TabIndex = 5;
             this.label3.Text = "New Password : ";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(309, 273);
+            this.textBox3.Location = new System.Drawing.Point(232, 222);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(202, 22);
+            this.textBox3.Size = new System.Drawing.Size(152, 20);
             this.textBox3.TabIndex = 6;
             this.textBox3.Leave += new System.EventHandler(this.textBox3_Leave);
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(309, 333);
+            this.textBox4.Location = new System.Drawing.Point(232, 271);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(202, 22);
+            this.textBox4.Size = new System.Drawing.Size(152, 20);
             this.textBox4.TabIndex = 7;
             this.textBox4.Leave += new System.EventHandler(this.textBox4_Leave);
             // 
@@ -131,9 +139,10 @@ namespace oop2Project
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(138, 338);
+            this.label4.Location = new System.Drawing.Point(104, 275);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(144, 17);
+            this.label4.Size = new System.Drawing.Size(116, 15);
             this.label4.TabIndex = 8;
             this.label4.Text = "Confirm  Password : ";
             // 
@@ -141,9 +150,10 @@ namespace oop2Project
             // 
             this.button2.Enabled = false;
             this.button2.Font = new System.Drawing.Font("Times New Roman", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(436, 373);
+            this.button2.Location = new System.Drawing.Point(327, 303);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.Size = new System.Drawing.Size(56, 24);
             this.button2.TabIndex = 9;
             this.button2.Text = "Confirm ";
             this.button2.UseVisualStyleBackColor = true;
@@ -153,18 +163,20 @@ namespace oop2Project
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(288, 26);
+            this.label5.Location = new System.Drawing.Point(216, 21);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(223, 32);
+            this.label5.Size = new System.Drawing.Size(177, 25);
             this.label5.TabIndex = 10;
             this.label5.Text = "Forget Password ";
             // 
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(25, 404);
+            this.button3.Location = new System.Drawing.Point(19, 328);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 34);
+            this.button3.Size = new System.Drawing.Size(56, 28);
             this.button3.TabIndex = 11;
             this.button3.Text = "Back ";
             this.button3.UseVisualStyleBackColor = true;
@@ -188,10 +200,10 @@ namespace oop2Project
             // 
             // Form7
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button2);
@@ -205,7 +217,9 @@ namespace oop2Project
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form7";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form7";
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
