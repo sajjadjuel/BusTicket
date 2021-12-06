3d1b7eeae020dce09e6515add6dcf7d5ddf54cde
sajjadjuel <63725506+sajjadjuel@users.noreply.github.com>
2021-12-07 00:00:12 +0600

    .

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,8 +1,17 @@
-﻿Create Table Bus (
+﻿Create Table Ticket (
+    TicketId VARCHAR (10) primary key NOT NULL,
+    Seat VARCHAR (10) NOT NULL,
+    Cus_Id VARCHAR (10) NOT NULL,
+    BusId VARCHAR (10) NOT NULL,
+    Date Date NOT NULL,
+    Time VARCHAR (10) NOT NULL,
+);
+Create Table Bus (
     BusId VARCHAR (25) primary key NOT NULL,
     BusName VARCHAR (25) NOT NULL,
     BusFrom VARCHAR (25) NOT NULL,
     BusTo VARCHAR (25) NOT NULL,
     Time VARCHAR (11) NOT NULL,
     TFormat VARCHAR (15) NOT NULL,
+    Fare VARCHAR (15) NOT NULL,
 );
